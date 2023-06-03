a178148196194eba67bf4b6986d773dcbff3c630
Trawirr <gtrawirr@gmail.com>
2023-06-03 23:49:17 +0200

    Memory class added

--- src/Memories/models.py
+++ src/Memories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Memory(models.Model):
+    rank = models.IntegerChoices("Rank", "DORMANT AWAKENED ASCENDED TRANSCENDENT SUPREME SACED DIVINE")
+    tier = models.IntegerChoices("Tier", "I II III IV V VI VII")
+    type = models.TextChoices("Type", "WEAPON RANGED_WEAPON ARMOR UTILITY")
+    description = models.TextField(default="A memory...")
+    is_usable = models.BooleanField(default=False)
+    uses = models.IntegerField(default=0)
+
+    # battle attributes
+    attack = models.IntegerField(default=0)
+    defense = models.IntegerField(default=0)
+    dodge = models.IntegerField(default=0)
+    initiative = models.IntegerField(default=0)
+
+    # rest attributes
+    heal = models.IntegerField(default=0)
+    heal_percent = models.FloatField(default=0.0)
