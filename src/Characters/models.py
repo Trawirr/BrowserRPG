a6d19f9517345b49b099f500d5c7ccb16514e5ab
Trawirr <gtrawirr@gmail.com>
2023-06-06 08:54:11 +0200

    User and Character used as foreign keys

--- src/Characters/models.py
+++ src/Characters/models.py
@@ -1,29 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Character(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    DORMANT = "DO"
-    AWAKENED = "AW"
-    ASCENDED = "AS"
-    TRANSCENDENT = "TR"
-    SUPREME = "SU"
-    SACRED = "SA"
-    DIVINE = "DI"
-    RANK_CHOICES = {
-        (DORMANT, "Dormant"),
-        (AWAKENED, "Awakened"),
-        (ASCENDED, "Ascended"),
-        (TRANSCENDENT, "Transcendent"),
-        (SUPREME, "Supreme"),
-        (SACRED, "Sacred"),
-        (DIVINE, "Divine"),
-    }
-    rank = models.CharField(max_length=2, choices=RANK_CHOICES, default=DORMANT)
+    # DORMANT = "DO"
+    # AWAKENED = "AW"
+    # ASCENDED = "AS"
+    # TRANSCENDENT = "TR"
+    # SUPREME = "SU"
+    # SACRED = "SA"
+    # DIVINE = "DI"
+    # RANK_CHOICES = {
+    #     (DORMANT, "Dormant"),
+    #     (AWAKENED, "Awakened"),
+    #     (ASCENDED, "Ascended"),
+    #     (TRANSCENDENT, "Transcendent"),
+    #     (SUPREME, "Supreme"),
+    #     (SACRED, "Sacred"),
+    #     (DIVINE, "Divine"),
+    # }
+    # rank = models.CharField(max_length=2, choices=RANK_CHOICES, default=DORMANT)
+    rank = models.IntegerChoices("Rank", "DORMANT AWAKENED ASCENDED TRANSCENDENT SUPREME SACED DIVINE")
     shards = models.IntegerField(default=0)
     
 class Aspect(models.Model):
-    pass
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
 
 class Ability(models.Model):
-    pass
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
