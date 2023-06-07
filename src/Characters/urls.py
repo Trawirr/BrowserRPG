dd6f4d845faa5946a6dbb98d415bfc53170a5f5b
Trawirr <gtrawirr@gmail.com>
2023-06-07 20:32:31 +0200

    Characters urls added

--- src/Characters/urls.py
+++ src/Characters/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.hello, name='hello'),
+    path('profil', views.index, name='character'),
+    path('login', views.login_view, name='login'),
+    path('register', views.register, name='register'),
 ]
