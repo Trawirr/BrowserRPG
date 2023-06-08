e13e7f9eda83dbdc8ecdf354901bcedc03df7be1
Trawirr <gtrawirr@gmail.com>
2023-06-08 20:19:14 +0200

    Models refactor

--- src/Characters/models.py
+++ src/Characters/models.py
@@ -1,34 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
+from RanksClasses.models import *
 # Create your models here.
 
 class Character(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    class Ranks(models.IntegerChoices):
-        DORMANT = 1, 'Dormant'
-        AWAKENED = 2, 'Awakened'
-        ASCENDED = 3, 'Ascended'
-        TRANSCENDENT = 4, 'Transcendent'
-        SUPREME = 5, 'Supreme'
-        SACRED = 6, 'Sacred'
-        DIVINE = 7, 'Divine'
-    # RANK_CHOICES = {
-    #     (DORMANT, "Dormant"),
-    #     (AWAKENED, "Awakened"),
-    #     (ASCENDED, "Ascended"),
-    #     (TRANSCENDENT, "Transcendent"),
-    #     (SUPREME, "Supreme"),
-    #     (SACRED, "Sacred"),
-    #     (DIVINE, "Divine"),
-    # }
-    # rank = models.CharField(max_length=2, choices=RANK_CHOICES, default=DORMANT)
-#    rank_choices = models.IntegerChoices("Rank", "DORMANT AWAKENED ASCENDED TRANSCENDENT SUPREME SACED DIVINE")
-    rank = models.IntegerField(choices=Ranks.choices)
+    rank = models.ForeignKey(Rank, on_delete=models.SET_NULL, null=True)
+    soul_core = models.IntegerField(default=0)
     shards = models.IntegerField(default=0)
-    
-class Aspect(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
 
-class Ability(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f"{self.user.username} {self.rank.name} {self.soul_core} {self.shards}"
+
+# Abstract superclass for Aspect, Ability and Flaw models
+class AbilitySuperclass(models.Model):
+    class Meta:
+        abstract = True
+
+    description = models.TextField(default="Description...")
+
+    # battle attributes
+    attack = models.IntegerField(default=0)
+    defense = models.IntegerField(default=0)
+    agility = models.IntegerField(default=0)
+    speed = models.IntegerField(default=0)
+
+    # rest attributes
+    heal = models.IntegerField(default=0)
+    heal_percent = models.FloatField(default=0.0)
+
+    # expedition attributes
+    passage = models.IntegerField(default=0)
+    stealth = models.IntegerField(default=0)
+    
+class Aspect(AbilitySuperclass):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="aspects")
+    rank = models.ForeignKey(Rank, on_delete=models.SET_NULL, null=True)
+
+    # AKA luck
+    prophecy = models.IntegerField(default=0)
+
+class Ability(AbilitySuperclass):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="abilities")
+
+class Flaw(AbilitySuperclass):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name="flaws")
