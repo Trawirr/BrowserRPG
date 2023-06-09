b8c3f1277d046294c73f123e95ad0fc50680bf60
Trawirr <gtrawirr@gmail.com>
2023-06-09 23:52:10 +0200

    some utils added

--- src/Characters/utils/utils.py
+++ src/Characters/utils/utils.py
@@ -2,10 +2,11 @@ from Characters.models import *
 import string
 import random
 
-def create_random_name():
-    return random.choice(string.ascii_uppercase) + ''.join(random.choices(string.ascii_lowercase, k=random.randint(2,10)))
+def create_random_sentence(n):
+    sentence = ' '.join([''.join(random.choices(string.ascii_lowercase, k=random.randint(2,10))) for i in range(n)])
+    return sentence[0].upper() + sentence[1:]
 
-def create_aspect(rank):
+def create_aspect_attributes(rank):
     points = 7+rank
     max_points = points
     attributes = [0 for i in range(8)]
@@ -19,16 +20,8 @@ def create_aspect(rank):
         points -= value
     return attributes
 
-def create_ability(attributes):
-    r = random.randint(1, sum(attributes))
-    for i in range(len(attributes)):
-        if sum(attributes[:i+1]) >= r: return i, random.randint(2,3)
-
-def create_flaw(rank):
-    return random.randint(0,7), -random.randint(1, rank)
-
-def test_aspect(rank):
-    attrs = create_aspect(rank)
+def create_aspect_dict(rank):
+    attrs = create_aspect_attributes(rank)
     fields = Aspect._meta.get_fields()
     fields = fields[2:10]
     print(attrs, [f.name for f in fields], sep='\n')
@@ -36,3 +29,11 @@ def test_aspect(rank):
     for i in range(8):
         d[fields[i].name] = attrs[i]
     return d
+
+def create_ability(attributes):
+    r = random.randint(1, sum(attributes))
+    for i in range(len(attributes)):
+        if sum(attributes[:i+1]) >= r: return i, random.randint(2,3)
+
+def create_flaw(rank):
+    return random.randint(0,7), -random.randint(1, rank)
