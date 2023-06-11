178cb4aa93b2985a2d39a3afff2c285311ed2c1d
Trawirr <gtrawirr@gmail.com>
2023-06-11 11:07:09 +0200

    Flaw added to admin view

--- src/Characters/admin.py
+++ src/Characters/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Characters.models import *
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Aspect)
+admin.site.register(Ability)
+admin.site.register(Flaw)
