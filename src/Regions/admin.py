b8d40b9257863d3d911aebbff5fa4c281dbb8d20
Trawirr <gtrawirr@gmail.com>
2023-06-13 12:08:57 +0200

    Region model created with custom methods and columns in admin view

--- src/Regions/admin.py
+++ src/Regions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from Regions.models import *
 
 # Register your models here.
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "gate", "count_adjacent", "distance_to_gate")
+
+    def count_adjacent(self, obj):
+        return len(obj.adjacent_regions.all())
+    
+    def distance_to_gate(self, obj):
+        return obj.distance_to_gate
--- src/Regions/models.py
+++ src/Regions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=50, default="New region...")
+    gate = models.BooleanField(default=False)
+    adjacent_regions = models.ManyToManyField('self', null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} {'*' if self.gate else ''}"
+    
+    @property
+    def distance_to_gate(self) -> int:
+        distance = 0
+        visited = []
+        to_visit = [self]
+        while to_visit:
+            to_visit_new = []
+            for region in to_visit:
+                if region.gate: 
+                    return distance
+                
+                visited.append(region)
+                for r in region.adjacent_regions.all():
+                    if r not in visited: to_visit_new.append(r)
+            to_visit = to_visit_new
+            distance += 1
