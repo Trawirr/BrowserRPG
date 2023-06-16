df744917dce574f9522d9f52f5e8921c9700744b
Trawirr <gtrawirr@gmail.com>
2023-06-16 23:38:06 +0200

    Enemies app updated

--- src/Enemies/models.py
+++ src/Enemies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from RanksClasses.models import *
 from Regions.models import *
+import random
 
 # Create your models here.
 class Enemy(models.Model):
@@ -17,3 +18,19 @@ class Enemy(models.Model):
     defense = models.IntegerField(default=0)
     agility = models.IntegerField(default=0)
     speed = models.IntegerField(default=0)
+    
+    @property
+    def all_attributes(self):
+        return {'attack': self.attack, 'defense': self.defense, 'agility': self.agility, 'speed': self.speed}
+    
+    def get_attack(self):
+        return max(1, random.randint(0, self.attributes.attack))
+    
+    def get_defense(self):
+        return self.attributes.defense
+    
+    def get_agility(self):
+        return self.attributes.agility
+    
+    def get_speed(self):
+        return max(1, random.randint(0, self.attributes.speed))
