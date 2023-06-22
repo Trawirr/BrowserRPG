c1015d63a50d6b7cfca22d7374a137e2daf4b679
Trawirr <gtrawirr@gmail.com>
2023-06-22 23:07:09 +0200

    RanksClasses updated

--- src/RanksClasses/admin.py
+++ src/RanksClasses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from RanksClasses.models import *
 
 # Register your models here.
+admin.site.register(CreatureRank)
+admin.site.register(CreatureClass)
+admin.site.register(Rank)
+admin.site.register(Tier)
--- src/RanksClasses/models.py
+++ src/RanksClasses/models.py
@@ -5,6 +5,9 @@ class Rank(models.Model):
     value = models.IntegerField(default=1, unique=True)
     name = models.CharField(max_length=12, default="Dormant", unique=True)
 
+    def __str__(self) -> str:
+        return f"{self.name} ({self.value})"
+
     def get_next(self):
         if self.value < 7:
             return Rank.objects.get(value=self.value + 1)
@@ -14,11 +17,24 @@ class CreatureRank(models.Model):
     value = models.IntegerField(default=1, unique=True)
     name = models.CharField(max_length=12, unique=True)
 
+    def __str__(self) -> str:
+        return f"{self.name} ({self.value})"
+
     def get_next(self):
         if self.value < 7:
             return CreatureRank.objects.get(value=self.value + 1)
         return None
     
-class Tier(models.model):
+class CreatureClass(models.Model):
+    value = models.IntegerField(default=1, unique=True)
+    name = models.CharField(max_length=12, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.value})"
+
+class Tier(models.Model):
     value = models.IntegerField(default=1, unique=True)
     name = models.CharField(max_length=4, unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.value})"
